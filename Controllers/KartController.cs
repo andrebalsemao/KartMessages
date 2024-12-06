2860cb38238fba33a75216fbff16cc8ade3f3aa3
andre.balsemao <andre.balsemao@primeglobal.com.br>
2024-12-05 21:53:14 -0300

    Add project files.

---
+++ Controllers/KartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KartMessages.Models;
+
+namespace KartMessages.Controllers
+{
+    public class KartController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new KartMessage());
+        }
+
+        [HttpGet]
+        public IActionResult DisplayMessage(int? KartNumber, string Message, bool? clear)
+        {
+            var model = new KartMessage
+            {
+                KartNumber = KartNumber ?? 0,
+                Message = clear == true ? "" : Message ?? ""
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/KartMessage.cs
@@ -0,0 +1,10 @@
+namespace KartMessages.Models
+{
+    public class KartMessage
+    {
+        public int KartNumber { get; set; }
+        public string Action { get; set; }
+        public string Laps { get; set; }
+        public string Message { get; set; }
+    }
+}
